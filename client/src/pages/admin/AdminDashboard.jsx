3f8140df725ff5261a93b36ae48ae04f203fe763
hardikpatil.hardikpatil894800@gmail.com <hardikpatil@hardikpatil894800.com>
2026-04-20 23:28:04 +0530

    updated env

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
-const API = "http://localhost:5000";
+const API = import.meta.env.VITE_API_URL;
 
 const StatCard = ({ title, value, subtitle }) => {
   return (
